39da131b96e6a5c5e391d08aa0cc984b32fb8557
edgarshang <edgarshang@163.com>
2019-03-16 10:16:36 +0800

    add extrao

--- main.cpp
+++ main.cpp
@@ -17,9 +17,30 @@ public:
     }
 };
 
+void Demo1()
+{
+    try {
+        throw 'c';
+    } catch (int)
+    {
+        cout << "catch(int i) " << endl;
+    }
+    catch (double d)
+    {
+        cout << "catch(double d)" << endl;
+    }
+    catch (char c)
+    {
+        cout << "catch(char i)" << endl;
+    }
+}
+
+
 int main()
 {
     cout << "Hello World!" << endl;
+
+    Demo1();
 
     SmartPointer<Test> sp = new Test();
     return 0;
