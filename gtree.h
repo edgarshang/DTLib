de63f919333c2836f7b745982498022081ddd181
edgarshang <edgarshang@163.com>
2019-08-19 23:08:57 +0800

    add find for tree

--- gtree.h
+++ gtree.h
@@ -10,34 +10,76 @@ namespace DTLib
 template <typename T>
 class GTree : public Tree<T>
 {
+protected:
+    GTreeNode<T>* find(GTreeNode<T>* node, const T& value) const
+    {
+        GTreeNode<T>* ret = nullptr;
+        if(node != nullptr)
+        {
+            if(node->value == value)
+            {
+                return node;
+            }else {
+                for (node->child.move(0); !node->child.end() && (ret == nullptr); node->child.next())
+                {
+                    ret = find(node->child.current(), value);
+                }
+            }
+        }
+
+        return ret;
+    }
+
+    GTreeNode<T>* find(GTreeNode<T>* node, GTreeNode<T>* obj) const
+    {
+        GTreeNode<T>* ret = nullptr;
+
+        if(node == obj)
+        {
+            return node;
+        }else {
+            if(node != nullptr)
+            {
+                for (node->child.move(0); !node->child.end() && (ret == nullptr); node->child.next())
+                {
+                        ret = find(node->child.current(), obj);
+                }
+            }
+        }
+
+        return ret;
+    }
 public:
     bool insert(TreeNode<T>* node)
     {
         bool ret = true;
-
+       find(dynamic_cast<GTreeNode<T>*>(node), 0);
         return ret;
     }
     bool insert(const T& value, TreeNode<T>* parent)
     {
         bool ret = true;
+         find(dynamic_cast<GTreeNode<T>*>(parent), value);
 
         return ret;
     }
    SharedPointer< Tree<T>> remove(const T& value)
    {
+       find(nullptr, value);
        return nullptr;
    }
     SharedPointer< Tree<T>> remove(TreeNode<T>* node)
    {
+         find(dynamic_cast<GTreeNode<T>*>(node), 0);
        return nullptr;
    }
     GTreeNode<T>* find(const T& value)
    {
-        return nullptr;
+        return find(root(), value);
    }
     GTreeNode<T>* find(TreeNode<T>* node)
    {
-        return nullptr;
+        return find(root(), dynamic_cast<GTreeNode<T>*>(node));
    }
    GTreeNode<T>* root()
    {
@@ -46,19 +88,27 @@ public:
 
    int degree()
    {
+       root();
        return 0;
    }
      int count()
    {
+         root();
          return 0;
    }
     int height()
    {
+        root();
         return 0;
    }
    void clear()
    {
         this->m_root = nullptr;
+   }
+
+   GTree()
+   {
+
    }
 
    ~GTree()
